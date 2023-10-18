b52dc843677f841e4891a8b485ae4a0a9b60bf04
katarzyna.pabian <katarzyna.pabian@SF-04>
2023-10-18 15:47:33 +0200

    Lab 3

---
+++ Lab 3 - App/Controllers/ContactController.cs
@@ -0,0 +1,70 @@
+﻿using Lab_3___App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_3___App.Controllers
+{
+    public class ContactController : Controller
+    {
+        //lista kontaktów
+        static Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+        static int id = 1;
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if(ModelState.IsValid)
+            {
+                //dodaj model do bazy lub kolekcji
+                model.Id = id++;
+                _contacts.Add(model.Id, model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_contacts[id]);
+        }
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                //dodaj model do bazy lub kolekcji
+                _contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(_contacts[id]);
+        }
+        [HttpPost]
+        public IActionResult Delete(Contact model)
+        {
+            _contacts.Remove(model.Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            return View(_contacts[id]);
+        }
+    }
+}
