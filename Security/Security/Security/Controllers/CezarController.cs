b3d3ab0d7e77dc8721b651fc02e977ea66789202
kpabian <katarzyna.pabian357@gmail.com>
2023-10-20 23:24:15 +0200

    Cezar 1.0

---
+++ Security/Security/Security/Controllers/CezarController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Security.Models;
+using System.Diagnostics;
+
+namespace Security.Controllers
+{
+    public class CezarController : Controller
+    {
+        private readonly ILogger<CezarController> _logger;
+
+        public CezarController(ILogger<CezarController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Result([FromForm] Cezar model)
+        {
+
+            if (!model.IsValid())
+                return View("Result");
+            return View(model);
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Security/Security/Security/Models/Cezar.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace Security.Models
+{
+    public class Cezar
+    {
+        public String Text { get; set; }
+        public int Shift { get; set; }
+
+
+        public bool IsValid()
+        {
+            return Text != null && Shift > 0;
+        }
+
+        public string EncryptingCezar()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in Text)
+            {
+                int a = c + Shift;
+                if(a )
+                sb = sb.Append(c + Shift);
+
+            }
+            return "";
+        }
+
+
+    }
+}
